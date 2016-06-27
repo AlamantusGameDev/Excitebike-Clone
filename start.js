1a68ef2ab4a5d40b7e918ad95da98557067ba0ac
Robbie Antenesse <gamedev@alamantus.com>
2016-06-27 15:54:55 -0600

    Updated OversimplifiedJS, added some basic settings.

--- start.js
+++ start.js
@@ -1,3 +1,14 @@
+OS.S.defaultStep = 1 / 30;
+OS.S.numberOfScriptsToLoad = 1;
+
+// Oversimplified.DEBUG.showMessages = false;
+OS.SetCamera({
+	width: 640,
+	height: 480,
+	hBorder: 64,
+	vBorder: 48
+});
+
 function start () {
 
 }
